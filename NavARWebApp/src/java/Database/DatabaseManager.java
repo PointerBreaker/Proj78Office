2d8536239f1f13aaa43f51aca2310219fdb8eb6c
Elize <0891435@hr.nl>
2016-05-06 10:57:12 +0200

    added dbmanager class

---
+++ NavARWebApp/src/java/Database/DatabaseManager.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+/**
+ *
+ * @author Elize
+ */
+public class DatabaseManager {
+    
+    /**
+     * 
+     * @return true if succesful
+     */
+    public boolean connectToDatabase(){
+    
+        return false;
+    }
+    
+    
+}
