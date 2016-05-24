bd3291a98c0762c3777f645e9dcde868a509430b
Elize <0891435@hr.nl>
2016-05-24 12:59:12 +0200

    put verandert in get

--- NavARWebApp/src/java/WebService/CustomerCheckInService.java
+++ NavARWebApp/src/java/WebService/CustomerCheckInService.java
@@ -5,6 +5,7 @@
  */
 package WebService;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
@@ -17,7 +18,7 @@ import javax.ws.rs.QueryParam;
 public class CustomerCheckInService {
     
     //TODO do somthing with the scanned meetingID?
-    @PUT
+    @GET
     @Path("putScannedMeetingId")
     public String putScannedMeetingId(@QueryParam("meetingId") int meetingId){
         return "Hello QR-scanner!";    
