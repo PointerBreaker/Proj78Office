5c77b8caa76da68ed515ffb78dfc6fee8d45214f
Rianne S. <0896535@hr.nl>
2016-06-04 13:59:26 +0200

    Mailgebeuren een beetje opgeruimd. (stuurt wel alleen nog maar mailtjes naar mij :P)

--- NavARWebApp/src/java/WebService/EmailSenderClass.java
+++ NavARWebApp/src/java/WebService/EmailSenderClass.java
@@ -33,37 +33,6 @@ public class EmailSenderClass {
 //    private Session mailSession;
     
     public void sendEmail(String recipientEmailAddress){     
-        /*try{
-            Properties props = System.getProperties();
-            props.setProperty("mail.smtp.host", applicationEmailHost);
-            props.setProperty("mail.smtp.port", "587");
-            props.put("mail.smtp.user", "navar.tamtam@gmail.com");
-            props.put("mail.smtp.password", "TamTamTam");
-            Session mailSession = Session.getDefaultInstance(props);   
-            
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(mailSession);
-            message.setFrom(new InternetAddress(applicationEmailAddress));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipientEmailAddress));
-            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-            Date date = new Date();
-            message.setSubject("A customer has arrived for you at " + dateFormat.format(date));
-            message.setContent("<h1>A customer has arrived for you!</h1>", "text/html");
-            
-            System.out.println("*R*: voor send");
-            
-            Transport transport = mailSession.getTransport("smtp");
-            transport.connect(applicationEmailHost, applicationEmailAddress, applicationEmailPassword);
-            transport.sendMessage(message, message.getAllRecipients());
-            //transport.send(message);
-            transport.close();
-            
-            System.out.println("*R*: na send");
-        } catch (MessagingException ex) {
-            System.out.println("*R*: exception");
-            Logger.getLogger(EmailSenderClass.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
-        
         try{
             Properties props = System.getProperties();
             props.put("mail.smtp.starttls.enable", "true");
@@ -78,23 +47,16 @@ public class EmailSenderClass {
             MimeMessage message = new MimeMessage(mailSession);
             message.setFrom(new InternetAddress(applicationEmailAddress));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipientEmailAddress));
-            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
             Date date = new Date();
             message.setSubject("Your client has arrived!");
             message.setContent("<h1>A client has arrived for your at " + dateFormat.format(date) + ".</h1>", "text/html");
 
             Transport transport = mailSession.getTransport("smtp");
             transport.connect(applicationEmailHost, applicationEmailAddress, applicationEmailPassword);
-            
-            System.out.println("*R*: " + message.getAllRecipients()[0]);
-            System.out.println("*R*: voor send");
-            
             transport.sendMessage(message, message.getAllRecipients());
             transport.close();
-            
-            System.out.println("*R*: na send");
         } catch (MessagingException ex) {
-            System.out.println("*R*: exception");
             Logger.getLogger(EmailSenderClass.class.getName()).log(Level.SEVERE, null, ex);
         }
     } 
