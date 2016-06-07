ed76507ab6defb3b0278bfaf5eeded46e66854f7
Elize <0891435@hr.nl>
2016-06-07 11:59:46 +0200

    compay meegegeven aan meeting + email gewijzigt

--- NavARWebApp/src/java/WebService/EmployeesService.java
+++ NavARWebApp/src/java/WebService/EmployeesService.java
@@ -121,7 +121,16 @@ public class EmployeesService {
         em.clear();
         em.close();        
         JSONObject json = JSONManager.getJSONObjectByList(results, "meetingRooms");
-        json.put("company_details", DatabaseManager.getCompanyById((int) json.get("company_id")));      
+        Companies c = DatabaseManager.getCompanyById((int) json.get("company_id"));
+            if(c != null){
+                try{
+                    JSONParser jsonParser = new JSONParser();
+                    JSONObject companyJSON = (JSONObject) jsonParser.parse(c.toString());
+                    json.put("company_details", companyJSON);      
+                } catch (ParseException ex) {
+                    Logger.getLogger(MeetingsService.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }    
         return json.toJSONString();
     }
 }
